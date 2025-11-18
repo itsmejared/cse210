120ff5869dedae863e0a00d6350c37b6b8875cbb
Jared Huayta <jared.huayta@qualifacts.com>
2025-11-18 04:59:24 -0500

    W04 Assignment: YouTube Video Program

--- week04/YouTubeVideos/Program.cs
+++ week04/YouTubeVideos/Program.cs
@@ -1,9 +1,34 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the YouTubeVideos Project.");
+        List<Video> videos = new List<Video>();
+
+        Video video1 = new Video("Cocina Peruana con Pollo", "El Cholo Mena", 400);
+        video1.AddComment(new Comment("Luis", "Amazing recipe!"));
+        video1.AddComment(new Comment("Mike", "Super clear explanation."));
+        video1.AddComment(new Comment("Andrés", "Amazing Flavor. Looks good."));
+        videos.Add(video1);
+
+        Video video2 = new Video("10 Money Hacks", "MoneySaver", 600);
+        video2.AddComment(new Comment("Carlos", "This helped me make more money."));
+        video2.AddComment(new Comment("Pedro", "Great tips!"));
+        video2.AddComment(new Comment("Vanessa", "Very helpful."));
+        videos.Add(video2);
+
+        Video video3 = new Video("Learn how to ride a bike", "Dad for you", 900);
+        video3.AddComment(new Comment("Luis", "I finally get it."));
+        video3.AddComment(new Comment("Marta", "Thank you so much!"));
+        video3.AddComment(new Comment("Ana", "Very easy to follow.")); 
+        videos.Add(video3);
+
+        foreach (Video v in videos)
+        {
+            v.DisplayInfo();
+            Console.WriteLine();
+        }
     }
 }
---
+++ week04/YouTubeVideos/Video.cs
@@ -0,0 +1,41 @@
+using System.Collections.Generic;
+
+public class Video
+{
+    private string _title;
+    private string _author;
+    private int _lengthSeconds;
+    private List<Comment> _comments;
+
+    public Video(string title, string author, int lengthSeconds)
+    {
+        _title = title;
+        _author = author;
+        _lengthSeconds = lengthSeconds;
+        _comments = new List<Comment>();
+    }
+
+    public void AddComment(Comment comment)
+    {
+        _comments.Add(comment);
+    }
+
+    public int GetCommentCount()
+    {
+        return _comments.Count;
+    }
+
+    public void DisplayInfo()
+    {
+        Console.WriteLine($"Title: {_title}");
+        Console.WriteLine($"Author: {_author}");
+        Console.WriteLine($"Length: {_lengthSeconds} seconds");
+        Console.WriteLine($"Comments: {GetCommentCount()}");
+        Console.WriteLine("Comments list:");
+
+        foreach (Comment c in _comments)
+        {
+            Console.WriteLine($" - {c.GetAuthorName()}: {c.GetText()}");
+        }
+    }
+}
