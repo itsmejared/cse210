b5b9dadcdd138ec78ae1bc648eca16e839c1dbc4
Jared Huayta <jared.huayta@qualifacts.com>
2025-12-09 16:40:18 -0500

    week07

---
+++ week07/ExerciseTracking/Activity.cs
@@ -0,0 +1,33 @@
+public abstract class Activity
+{
+    private string _date;
+    private int _length;
+
+    public Activity(string date, int length)
+    {
+        _date = date;
+        _length = length;
+    }
+
+    public string GetDate()
+    {
+        return _date;
+    }
+
+    public int GetLength()
+    {
+        return _length;
+    }
+
+    public abstract double GetDistance();
+    public abstract double GetSpeed();
+    public abstract double GetPace();
+
+    public virtual string GetSummary()
+    {
+        return $"{_date} {this.GetType().Name} ({_length} min) - " +
+               $"Distance: {GetDistance():0.0} miles, " +
+               $"Speed: {GetSpeed():0.0} mph, " +
+               $"Pace: {GetPace():0.0} min per mile";
+    }
+}
